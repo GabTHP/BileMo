b734d17f2f8fee9714e13840164db1ca26b0afea
GabTHP <gabriel.bouakira@hotmail.fr>
2022-03-31 19:50:59 +0200

    api add user to customer et delete user to customer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
+use App\Repository\CustomerRepository;
+use App\Entity\User;
+
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @Route("/api", name="api_user")
@@ -14,7 +18,7 @@ use App\Repository\UserRepository;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/customers/{customer_id}/users", name="all_consumer_users")
+     * @Route("/customers/{customer_id}/users", name="all_consumer_users", methods={"GET"})
      */
     public function index(UserRepository $repo, $customer_id): Response
     {
@@ -35,16 +39,16 @@ class UserController extends AbstractController
         return $this->json($data);
     }
 
-        /**
-     * @Route("/customers/{customer_id}/users/{user_id}", name="single_consumer_users")
+    /**
+     * @Route("/customers/{customer_id}/users/{user_id}", name="single_consumer_users", methods={"GET"})
      */
     public function show(UserRepository $repo, $customer_id, $user_id): Response
     {
         $user = $repo->findOneBy(['id' => $user_id]);
 
-        if (!$product) {
+        if (!$user) {
  
-            return $this->json('Aucun utilisateur ne correspond à cet id' . $id, 404);
+            return $this->json('Aucun utilisateur ne correspond à cet id' . $user_id, 404);
         }
  
            $data[] = [
@@ -57,5 +61,43 @@ class UserController extends AbstractController
            ];
 
         return $this->json($data);
+    }
+
+    /**
+     * @Route("/customers/{customer_id}/users", name="user_new", methods={"POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $em, CustomerRepository $repo, $customer_id): Response
+    {
+        $user = new User();
+        $customer = $repo->findOneBy(['id' => $customer_id]);
+        $user->setUsername($request->request->get('username'));
+        $user->setFirstName($request->request->get('first_name'));
+        $user->setLastName($request->request->get('last_name'));
+        $user->setEmail($request->request->get('email'));
+        $user->setPassword($request->request->get('password'));
+        $user->setCustomer($customer);
+ 
+        $em->persist($user);
+        $em->flush();
+ 
+        return $this->json('Nouvel utilisateur créé avec succés, son id est le : ' . $user->getId());
+    }
+
+    /**
+     * @Route("/customers/{customer_id}/users/{user_id}", name="delete_consumer_user", methods={"DELETE"})
+     */
+    public function delete(UserRepository $repo, EntityManagerInterface $em, $customer_id, $user_id): Response
+    {
+        $user = $repo->findOneBy(['id' => $user_id]);
+
+        if (!$user) {
+ 
+            return $this->json('Aucun utilisateur ne correspond à cet id' . $user_id, 404);
+        }
+ 
+        $em->remove($user);
+        $em->flush();
+
+        return $this->json('Utilisateur supprimé');
     }
 }
