3fc0bcc4febee5d23b8864efdd9e3abb1e98ffb9
GabTHP <gabriel.bouakira@hotmail.fr>
2022-03-31 17:18:27 +0200

    product api ok

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,16 +5,60 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 
+/**
+ * @Route("/api", name="api_")
+ */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product", name="app_product")
+     * @Route("/products", name="all_products")
      */
-    public function index(): Response
+    public function index(ProductRepository $repo): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $products = $repo->findAll();
+ 
+        $data = [];
+ 
+        foreach ($products as $product) {
+           $data[] = [
+               'id' => $product->getId(),
+               'name' => $product->getName(),
+               'description' => $product->getDescription(),
+               'brand' => $product->getBrand(),
+               'model' => $product->getModel(),
+               'created_at' => $product->getCreatedAt(),
+               'updated_at' => $product->getUpdatedAt(),
+           ];
+        }
+        return $this->json($data);
+    }
+
+    /**
+     * @Route("/product/{id}", name="product_show", methods={"GET"})
+     */
+    public function show(ProductRepository $repo, $id): Response
+    {
+        $product = $repo->findOneBy(['id' => $id]);
+ 
+        if (!$product) {
+ 
+            return $this->json('Aucun ne produit ne correspond à cet id' . $id, 404);
+        }
+ 
+        $data =  [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'description' => $product->getDescription(),
+            'brand' => $product->getBrand(),
+            'model' => $product->getModel(),
+            'created_at' => $product->getCreatedAt(),
+            'updated_at' => $product->getUpdatedAt(),
+        ];
+         
+        return $this->json($data);
     }
 }
