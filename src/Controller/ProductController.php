692247c8d16a17bfc0f6fa3cb38fef170748adf9
Gabriel Bouakira <gb@ipsip.eu>
2022-05-12 09:50:06 +0200

    expose json page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation as Doc;
+
 
 
 use App\Repository\ProductRepository;
