da93b8c1249905075e51cd3aecb1658061ccd120
Gabriel Bouakira <gb@ipsip.eu>
2022-05-12 09:23:05 +0200

    composer install nelmio

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
